52e7f003c8dfce1ddfe007a3d11c91cfcd00063d
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-02-25 19:48:37 +0530

    foodie-App/lib/app_widget directory

--- Foodie-App/lib/app_widget/app_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'app_notifier.dart';
-import 'app_state.dart';
-
-final appProvider = StateNotifierProvider<AppNotifier, AppState>(
-  (ref) => AppNotifier(),
-);
