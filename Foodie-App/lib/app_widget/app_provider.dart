f27c4c64d25d0691519e6de8f90b7b54f63e1ece
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-02-25 18:55:18 +0530

    Create app_provider.dart

---
+++ Foodie-App/lib/app_widget/app_provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'app_notifier.dart';
+import 'app_state.dart';
+
+final appProvider = StateNotifierProvider<AppNotifier, AppState>(
+  (ref) => AppNotifier(),
+);
