43a3ae9662498dd5217e1b2b3e0465b8e65a5380
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-02-25 20:35:41 +0530

    Update order_status_item.dart

--- Foodie-App/lib/presentation/pages/order/order_screen/widgets/order_status_item.dart
+++ Foodie-App/lib/presentation/pages/order/order_screen/widgets/order_status_item.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:riverpodtemp/presentation/theme/theme.dart';
+import 'package:shoppingapp/presentation/theme/theme.dart';
 
 class OrderStatusItem extends StatelessWidget {
   final Widget icon;
