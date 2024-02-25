d8925a5905adb7aa304905f74c8af6e77c5544bf
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-02-25 20:32:42 +0530

    Update refund_screen.dart

--- Foodie-App/lib/presentation/pages/order/order_check/widgets/refund_screen.dart
+++ Foodie-App/lib/presentation/pages/order/order_check/widgets/refund_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:riverpodtemp/application/order/order_provider.dart';
-import 'package:riverpodtemp/infrastructure/services/app_helpers.dart';
-import 'package:riverpodtemp/infrastructure/services/tr_keys.dart';
-import 'package:riverpodtemp/presentation/components/title_icon.dart';
-import 'package:riverpodtemp/presentation/theme/app_style.dart';
+import 'package:shoppingapp/application/order/order_provider.dart';
+import 'package:shoppingapp/infrastructure/services/app_helpers.dart';
+import 'package:shoppingapp/infrastructure/services/tr_keys.dart';
+import 'package:shoppingapp/presentation/components/title_icon.dart';
+import 'package:shoppingapp/presentation/theme/app_style.dart';
 
 import '../../../../components/buttons/custom_button.dart';
 import '../../../../components/text_fields/outline_bordered_text_field.dart';
