ab4d03cb1643e2a91344b32edc17a8349f21042d
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-02-25 20:08:09 +0530

    Create .env.dart

---
+++ Foodie-App/lib/infrastructure/services/.env.dart
@@ -0,0 +1,12 @@
+const stripePublishableKey =
+    'pk_test_51LaGXyLrMlc0ddAAYc8wxwq8eFj9HbgpvValjxRtg2OZzTJDwFz0ZmATL1TOqAWUFBM3CeHbI5Cny71MzkrAXRxa00XTCJArPr';
+const tokenizationKey = 'sandbox_8hxpnkht_kzdtzv2btm4p7s5j';
+const clientToken = 'cc7spyb35msck2v5';
+const clientId =
+    'ATDP-sRgmqduMDVjO7T3pbn2milfHAzcClXfZY5DAt9WT-n8Fma33bQU--H0Qf6aMZDrfzKpS79GmQvm';
+const stripeSecretKey =
+    'sk_test_51LaGXyLrMlc0ddAAFGTcyYkpVuaASL6XyY0djSEbnbhjFmaD51n1SL7hakh7UkjubMnykJ1wrH15QfbEWN7byuIG00kMcA2yxg';
+const razorpayKey = 'rzp_test_TMPwvQpYGAIMbU';
+const payStackPublicKey = 'pk_test_2ec2bc5ed445385489385b2fce6cddfbfb265914';
+const clientIdPayPal = 'AfXG3cGfppuJm2tcvhZAedI1IipEqeNkBS1h0pYtT8h88RhYBj8quuMz6OOah5iBMxHDqbmd90AUSbnn';
+const secretKeyPayPal = 'EEiVoh2ZXma5k4BnLzc4ZXy3HGXkKwnCWoZrsCvFMV43uQnJk_eOu5fFl9TTVu0EHZS4TAxl9hGuqzYS';
