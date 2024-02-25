4a964c4f87708eaabe0b8e0ea90b7215be6cadc1
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-02-25 19:59:00 +0530

    profile files..

---
+++ Foodie-App/lib/application/profile/profile_state.freezed.dart
@@ -0,0 +1,379 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'profile_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$ProfileState {
+  bool get isLoading => throw _privateConstructorUsedError;
+  bool get isReferralLoading => throw _privateConstructorUsedError;
+  bool get isSaveLoading => throw _privateConstructorUsedError;
+  bool get isLoadingHistory => throw _privateConstructorUsedError;
+  int get typeIndex => throw _privateConstructorUsedError;
+  String get bgImage => throw _privateConstructorUsedError;
+  String get logoImage => throw _privateConstructorUsedError;
+  AddressData? get addressModel => throw _privateConstructorUsedError;
+  ProfileData? get userData => throw _privateConstructorUsedError;
+  ReferralModel? get referralData => throw _privateConstructorUsedError;
+  List<WalletData>? get walletHistory => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ProfileStateCopyWith<ProfileState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ProfileStateCopyWith<$Res> {
+  factory $ProfileStateCopyWith(
+          ProfileState value, $Res Function(ProfileState) then) =
+      _$ProfileStateCopyWithImpl<$Res, ProfileState>;
+  @useResult
+  $Res call(
+      {bool isLoading,
+      bool isReferralLoading,
+      bool isSaveLoading,
+      bool isLoadingHistory,
+      int typeIndex,
+      String bgImage,
+      String logoImage,
+      AddressData? addressModel,
+      ProfileData? userData,
+      ReferralModel? referralData,
+      List<WalletData>? walletHistory});
+}
+
+/// @nodoc
+class _$ProfileStateCopyWithImpl<$Res, $Val extends ProfileState>
+    implements $ProfileStateCopyWith<$Res> {
+  _$ProfileStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? isLoading = null,
+    Object? isReferralLoading = null,
+    Object? isSaveLoading = null,
+    Object? isLoadingHistory = null,
+    Object? typeIndex = null,
+    Object? bgImage = null,
+    Object? logoImage = null,
+    Object? addressModel = freezed,
+    Object? userData = freezed,
+    Object? referralData = freezed,
+    Object? walletHistory = freezed,
+  }) {
+    return _then(_value.copyWith(
+      isLoading: null == isLoading
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isReferralLoading: null == isReferralLoading
+          ? _value.isReferralLoading
+          : isReferralLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isSaveLoading: null == isSaveLoading
+          ? _value.isSaveLoading
+          : isSaveLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isLoadingHistory: null == isLoadingHistory
+          ? _value.isLoadingHistory
+          : isLoadingHistory // ignore: cast_nullable_to_non_nullable
+              as bool,
+      typeIndex: null == typeIndex
+          ? _value.typeIndex
+          : typeIndex // ignore: cast_nullable_to_non_nullable
+              as int,
+      bgImage: null == bgImage
+          ? _value.bgImage
+          : bgImage // ignore: cast_nullable_to_non_nullable
+              as String,
+      logoImage: null == logoImage
+          ? _value.logoImage
+          : logoImage // ignore: cast_nullable_to_non_nullable
+              as String,
+      addressModel: freezed == addressModel
+          ? _value.addressModel
+          : addressModel // ignore: cast_nullable_to_non_nullable
+              as AddressData?,
+      userData: freezed == userData
+          ? _value.userData
+          : userData // ignore: cast_nullable_to_non_nullable
+              as ProfileData?,
+      referralData: freezed == referralData
+          ? _value.referralData
+          : referralData // ignore: cast_nullable_to_non_nullable
+              as ReferralModel?,
+      walletHistory: freezed == walletHistory
+          ? _value.walletHistory
+          : walletHistory // ignore: cast_nullable_to_non_nullable
+              as List<WalletData>?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ProfileStateImplCopyWith<$Res>
+    implements $ProfileStateCopyWith<$Res> {
+  factory _$$ProfileStateImplCopyWith(
+          _$ProfileStateImpl value, $Res Function(_$ProfileStateImpl) then) =
+      __$$ProfileStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {bool isLoading,
+      bool isReferralLoading,
+      bool isSaveLoading,
+      bool isLoadingHistory,
+      int typeIndex,
+      String bgImage,
+      String logoImage,
+      AddressData? addressModel,
+      ProfileData? userData,
+      ReferralModel? referralData,
+      List<WalletData>? walletHistory});
+}
+
+/// @nodoc
+class __$$ProfileStateImplCopyWithImpl<$Res>
+    extends _$ProfileStateCopyWithImpl<$Res, _$ProfileStateImpl>
+    implements _$$ProfileStateImplCopyWith<$Res> {
+  __$$ProfileStateImplCopyWithImpl(
+      _$ProfileStateImpl _value, $Res Function(_$ProfileStateImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? isLoading = null,
+    Object? isReferralLoading = null,
+    Object? isSaveLoading = null,
+    Object? isLoadingHistory = null,
+    Object? typeIndex = null,
+    Object? bgImage = null,
+    Object? logoImage = null,
+    Object? addressModel = freezed,
+    Object? userData = freezed,
+    Object? referralData = freezed,
+    Object? walletHistory = freezed,
+  }) {
+    return _then(_$ProfileStateImpl(
+      isLoading: null == isLoading
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isReferralLoading: null == isReferralLoading
+          ? _value.isReferralLoading
+          : isReferralLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isSaveLoading: null == isSaveLoading
+          ? _value.isSaveLoading
+          : isSaveLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isLoadingHistory: null == isLoadingHistory
+          ? _value.isLoadingHistory
+          : isLoadingHistory // ignore: cast_nullable_to_non_nullable
+              as bool,
+      typeIndex: null == typeIndex
+          ? _value.typeIndex
+          : typeIndex // ignore: cast_nullable_to_non_nullable
+              as int,
+      bgImage: null == bgImage
+          ? _value.bgImage
+          : bgImage // ignore: cast_nullable_to_non_nullable
+              as String,
+      logoImage: null == logoImage
+          ? _value.logoImage
+          : logoImage // ignore: cast_nullable_to_non_nullable
+              as String,
+      addressModel: freezed == addressModel
+          ? _value.addressModel
+          : addressModel // ignore: cast_nullable_to_non_nullable
+              as AddressData?,
+      userData: freezed == userData
+          ? _value.userData
+          : userData // ignore: cast_nullable_to_non_nullable
+              as ProfileData?,
+      referralData: freezed == referralData
+          ? _value.referralData
+          : referralData // ignore: cast_nullable_to_non_nullable
+              as ReferralModel?,
+      walletHistory: freezed == walletHistory
+          ? _value._walletHistory
+          : walletHistory // ignore: cast_nullable_to_non_nullable
+              as List<WalletData>?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$ProfileStateImpl extends _ProfileState {
+  const _$ProfileStateImpl(
+      {this.isLoading = true,
+      this.isReferralLoading = true,
+      this.isSaveLoading = false,
+      this.isLoadingHistory = true,
+      this.typeIndex = 0,
+      this.bgImage = "",
+      this.logoImage = "",
+      this.addressModel = null,
+      this.userData = null,
+      this.referralData = null,
+      final List<WalletData>? walletHistory = const []})
+      : _walletHistory = walletHistory,
+        super._();
+
+  @override
+  @JsonKey()
+  final bool isLoading;
+  @override
+  @JsonKey()
+  final bool isReferralLoading;
+  @override
+  @JsonKey()
+  final bool isSaveLoading;
+  @override
+  @JsonKey()
+  final bool isLoadingHistory;
+  @override
+  @JsonKey()
+  final int typeIndex;
+  @override
+  @JsonKey()
+  final String bgImage;
+  @override
+  @JsonKey()
+  final String logoImage;
+  @override
+  @JsonKey()
+  final AddressData? addressModel;
+  @override
+  @JsonKey()
+  final ProfileData? userData;
+  @override
+  @JsonKey()
+  final ReferralModel? referralData;
+  final List<WalletData>? _walletHistory;
+  @override
+  @JsonKey()
+  List<WalletData>? get walletHistory {
+    final value = _walletHistory;
+    if (value == null) return null;
+    if (_walletHistory is EqualUnmodifiableListView) return _walletHistory;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(value);
+  }
+
+  @override
+  String toString() {
+    return 'ProfileState(isLoading: $isLoading, isReferralLoading: $isReferralLoading, isSaveLoading: $isSaveLoading, isLoadingHistory: $isLoadingHistory, typeIndex: $typeIndex, bgImage: $bgImage, logoImage: $logoImage, addressModel: $addressModel, userData: $userData, referralData: $referralData, walletHistory: $walletHistory)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ProfileStateImpl &&
+            (identical(other.isLoading, isLoading) ||
+                other.isLoading == isLoading) &&
+            (identical(other.isReferralLoading, isReferralLoading) ||
+                other.isReferralLoading == isReferralLoading) &&
+            (identical(other.isSaveLoading, isSaveLoading) ||
+                other.isSaveLoading == isSaveLoading) &&
+            (identical(other.isLoadingHistory, isLoadingHistory) ||
+                other.isLoadingHistory == isLoadingHistory) &&
+            (identical(other.typeIndex, typeIndex) ||
+                other.typeIndex == typeIndex) &&
+            (identical(other.bgImage, bgImage) || other.bgImage == bgImage) &&
+            (identical(other.logoImage, logoImage) ||
+                other.logoImage == logoImage) &&
+            (identical(other.addressModel, addressModel) ||
+                other.addressModel == addressModel) &&
+            (identical(other.userData, userData) ||
+                other.userData == userData) &&
+            (identical(other.referralData, referralData) ||
+                other.referralData == referralData) &&
+            const DeepCollectionEquality()
+                .equals(other._walletHistory, _walletHistory));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      isLoading,
+      isReferralLoading,
+      isSaveLoading,
+      isLoadingHistory,
+      typeIndex,
+      bgImage,
+      logoImage,
+      addressModel,
+      userData,
+      referralData,
+      const DeepCollectionEquality().hash(_walletHistory));
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ProfileStateImplCopyWith<_$ProfileStateImpl> get copyWith =>
+      __$$ProfileStateImplCopyWithImpl<_$ProfileStateImpl>(this, _$identity);
+}
+
+abstract class _ProfileState extends ProfileState {
+  const factory _ProfileState(
+      {final bool isLoading,
+      final bool isReferralLoading,
+      final bool isSaveLoading,
+      final bool isLoadingHistory,
+      final int typeIndex,
+      final String bgImage,
+      final String logoImage,
+      final AddressData? addressModel,
+      final ProfileData? userData,
+      final ReferralModel? referralData,
+      final List<WalletData>? walletHistory}) = _$ProfileStateImpl;
+  const _ProfileState._() : super._();
+
+  @override
+  bool get isLoading;
+  @override
+  bool get isReferralLoading;
+  @override
+  bool get isSaveLoading;
+  @override
+  bool get isLoadingHistory;
+  @override
+  int get typeIndex;
+  @override
+  String get bgImage;
+  @override
+  String get logoImage;
+  @override
+  AddressData? get addressModel;
+  @override
+  ProfileData? get userData;
+  @override
+  ReferralModel? get referralData;
+  @override
+  List<WalletData>? get walletHistory;
+  @override
+  @JsonKey(ignore: true)
+  _$$ProfileStateImplCopyWith<_$ProfileStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
